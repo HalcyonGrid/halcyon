a38154c17961b5861d6b5454dcbcdefc8e81475c
Paul Whittemore <dev@appurist.com>
2018-12-21 22:54:29 -0400

    Added ConsoleMode class and call to disable Quick Edit mode on consoles.
    Was causing consoles to hang if someone clicked on one with the mouse. (I thought this was already coded, it's like a change was lost.) This implementation is probably cleaner than any previous one anyway.

---
+++ OpenSim/Framework/Console/ConsoleMode.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace OpenSim.Framework.Console
+{
+    public static class ConsoleMode
+    {
+        const uint ENABLE_QUICK_EDIT = 0x0040;
+        const uint ENABLE_EXTENDED_FLAGS = 0x0080;
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern IntPtr GetStdHandle(int nStdHandle);
+
+        [DllImport("kernel32.dll")]
+        static extern bool GetConsoleMode(IntPtr hConsoleHandle, out uint lpMode);
+
+        [DllImport("kernel32.dll")]
+        static extern bool SetConsoleMode(IntPtr hConsoleHandle, uint dwMode);
+
+        private const int STDIN_HANDLE = -10;
+        internal static bool DisableQuickEdit()
+        {
+            IntPtr consoleHandle = GetStdHandle(STDIN_HANDLE);
+
+            uint consoleMode;
+            // get current console mode
+            if (!GetConsoleMode(consoleHandle, out consoleMode))
+                return false;
+
+            // setting QUICK_EDIT mode either way requires EXTENDED
+            consoleMode |= ENABLE_EXTENDED_FLAGS;
+            consoleMode &= ~ENABLE_QUICK_EDIT;
+            return SetConsoleMode(consoleHandle, consoleMode);
+        }
+    }
+}
